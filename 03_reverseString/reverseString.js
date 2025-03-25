05fefa804af9daa1d4b36e19b891f669faa6cf36
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-03-25 22:24:19 +0000

    finished reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (oldString) {
+  let newString = oldString.split("");
+  newString.reverse();
+  let newestString = newString.join("");
+  return newestString;
 };
 
 // Do not edit below this line
