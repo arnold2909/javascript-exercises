a46100ff06f19feb1b103dd29c8c71815fa2082a
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-03-25 22:16:32 +0000

    completed first couple of exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, repeatNumber) {
+  let newString = "";
+  for (let i = 1; i <= repeatNumber; i++) {
+    newString += string;
+  }
+  if (repeatNumber >= 0) {
+    return newString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
