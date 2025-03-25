c6828d73cc339743bfb0ec54a001cf62dd5a708a
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-03-25 22:54:57 +0000

    actually did exercise 5 -- previous commit was actually 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let total = 0;
+  if (
+    num2 >= num1 &&
+    num1 >= 0 &&
+    num2 >= 0 &&
+    num1 === parseInt(num1, 10) &&
+    num2 === parseInt(num2, 10)
+  ) {
+    for (let i = num1; i <= num2; i++) {
+      total += i;
+    }
+  } else if (
+    num2 <= num1 &&
+    num1 >= 0 &&
+    num2 >= 0 &&
+    num1 === parseInt(num1, 10) &&
+    num2 === parseInt(num2, 10)
+  ) {
+    for (let i = num2; i <= num1; i++) {
+      total += i;
+    }
+  } else {
+    return "ERROR";
+  }
 
+  return total;
 };
 
 // Do not edit below this line
