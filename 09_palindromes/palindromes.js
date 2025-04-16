215b9737f9de3c9989f29eeef32c9e8cfc450474
Arnold Popliceanu <popliceanu.arnold@gmail.com>
2025-04-16 14:47:10 +0100

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  if (
+    input
+      .toLowerCase()
+      .replace(/\s/g, "")
+      .split("")
+      .reverse()
+      .filter((char) => /^[A-Za-z0-9]*$/.test(char))
+      .join("") ===
+    input
+      .toLowerCase()
+      .replace(/\s/g, "")
+      .split("")
+      .filter((char) => /^[A-Za-z0-9]*$/.test(char))
+      .join("")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
